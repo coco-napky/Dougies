8f857842c67d97d551b629b4cd74f52a1677bb91
Dventura02 <douglas_v96@hotmail.com>
2015-12-26 15:38:25 -0600

    UI character select
    cambiar player pref de tortuga e instanciar dependiendo del pref

---
+++ Assets/Scripts/UI/GetChar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetChar : MonoBehaviour {
+    private int select;
+    public GameObject Dougie;
+    public GameObject Dougie2;
+    public GameObject Dougie3;
+    // Use this for initialization
+    void Start () {
+        select = PlayerPrefs.GetInt("Player Score");
+        GameObject tacoProjectile;
+        Vector3 offset;
+        offset =new Vector3(-10, -1.55f, 0);
+
+        switch(select)
+        {
+            case 1: 
+                tacoProjectile = (GameObject)Instantiate(Dougie, offset, transform.rotation);
+                break;
+            case 2:
+                tacoProjectile = (GameObject)Instantiate(Dougie2, offset, transform.rotation);
+                break;
+            default:
+                tacoProjectile = (GameObject)Instantiate(Dougie3, offset, transform.rotation);
+                break;
+        }
+        
+        tacoProjectile.GetComponent<Transform>().Rotate(0, 180, 0);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // print(PlayerPrefs.GetInt("Player Score"));
+    }
+}
---
+++ Assets/Scripts/UI/SelectChar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectChar : MonoBehaviour {
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode start;
+    public int select = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(up))
+        {
+            select++;
+            if (select > 2)
+                select = 1;
+        }
+
+        if (Input.GetKeyDown(down))
+        {
+            select--;
+            if (select < 1)
+                select = 2;
+        }
+
+        switch(select)
+        {
+            case 1:
+                PlayerPrefs.SetInt("Player Score", 1);
+                print("1");
+                break;
+            case 2:
+                PlayerPrefs.SetInt("Player Score", 2);
+                print("2");
+                break;
+
+
+        }
+
+        if(Input.GetKeyDown(start))
+        {
+            Application.LoadLevel("EmptyStage");
+        }
+
+
+        
+        
+    }
+}
